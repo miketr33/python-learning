497e44b30590ef3ef3948a3687c5e61a47fe260d
Michael Tree <mikejtree@googlemail.com>
2018-03-05 17:51:08 +0000

    Added printing in dif bases

---
+++ base_represenations.py
@@ -0,0 +1,11 @@
+#   One way to print binary in base 10
+print(0b1011)    # Should print 11
+
+#   To print base 10 in binary
+print(bin(11))
+
+#   To print base 10 in hex
+print(hex(11))
+
+#   To print base 10 in oct
+print(oct(11))
