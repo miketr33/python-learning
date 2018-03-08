3e5a37a3d779fa3d8c7d3587152dcc77749f23f0
Michael Tree <mikejtree@googlemail.com>
2018-03-08 15:34:19 +0000

    Demo by david
    of how you could read through files.
    
    Pseudo

---
+++ classes_and_lists.py
@@ -0,0 +1,41 @@
+# Demo by David
+
+class Storage ():
+    '''
+    class to store lots of lists
+    '''
+    # default variables
+    list1 = []
+    list2 = []
+    list3 = []
+
+    def __init__():
+         print('storage initialised...')
+
+    def emptyLists():
+        List1 = []
+
+myStorage = Storage()
+myStorage.list1.append('')
+myStorage.emptyLists()
+
+myDictStorage = {}
+myDictStorage['errors'] = []
+myDictStorage['List2'] = []
+
+class fileToTest ():
+    filename = ''
+    filepath = ''
+    errors = []
+    bit_depth = 10
+
+    def __init__():
+
+filesToTest = []
+
+
+for task in filesToTest:
+    filesToTest.append(fileToTest(''))
+#file.error
+#file.file_name
+fileToTest
