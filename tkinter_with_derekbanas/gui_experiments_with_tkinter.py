a7d7265c1f1522d1b1c01ba649f032fbe3122896
Michael Tree <mikejtree@googlemail.com>
2018-03-11 22:06:52 +0000

    more gui experis

--- tkinter_with_derekbanas/gui_experiments_with_tkinter.py
+++ tkinter_with_derekbanas/gui_experiments_with_tkinter.py
@@ -14,6 +14,11 @@ Label(root, text = "Options").grid(row = 0,column = 3, sticky = E, padx = 4)
 pathname_entry_but = Button(root, text = "Enter Pathname").grid(row = 1, column = 3, sticky = E, padx = 4)
 file_picker_but = Button(root, text = "Open File").grid(row = 2, column = 3, sticky = E, padx = 4)
 
+canvas = Canvas(root,width=200,height=150)
+canvas.grid(row = 0,column = 0, sticky = W, padx =10)
+canvas.create_rectangle(4,10,190,140,fill="Khaki3")
+#canvas=Canvas()
+#canvas.create_rectangle(10,10,210,210, dash = (4,2))   # Top line
 
-print(root.filedirectory)
+#print(root.filedirectory)
 root.mainloop()
